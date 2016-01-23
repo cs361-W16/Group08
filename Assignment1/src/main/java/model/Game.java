920f9af7af05f786f47a2061c70e170cfa0fa796
Emily Longman <longmane@oregonstate.edu>
2016-01-22 19:17:16 -0800

    added gitignore
    maven does weird things

--- Assignment1/src/main/java/model/Game.java
+++ Assignment1/src/main/java/model/Game.java
@@ -8,15 +8,6 @@ import java.util.ArrayList;
  * Created by Emily on 1/22/2016.
  */
 public class Game {
-    /**
-     * Created by Emily on 1/22/2016.
-     */
-    public static class Remove implements Serializable {
-
-        public void remove_card(card){
-
-        }
-    }
 
     /**
      * Created by Brandon on 1/21/2016.
@@ -25,11 +16,35 @@ public class Game {
         public ArrayList<ArrayList<Card>> table;
         public ArrayList<Card> column;
 
-        public TableCreator(){
+        public TableCreator() {
             table = new ArrayList<ArrayList<Card>>();
         }
-        public ArrayList<ArrayList<Card>> getTable(){
+
+        public ArrayList<ArrayList<Card>> getTable() {
             return table;
         }
+    }
+
+
+    /**
+     * Created by Emily on 1/22/2016.
+     */
+    public static class Remove implements Serializable {
+
+        public void remove_card(card) {
+
+        }
+    }
+
+
+    public void Deal(Deck dealt){
+
+        for(int i=0; i<4; i++) {
+
+            TableCreator.table.get(i).add(dealt.cards.get(0));
+            dealt.cards.remove(0);
+
+        }
+
     }
 }
