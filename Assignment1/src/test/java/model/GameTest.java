899a7a56e55343989cf9a14bb9aa77a4b280c342
Dioxu <ellis113@gmail.com>
2016-01-22 20:46:01 -0800

    An ugly. ugly. test suite for Dealing

--- Assignment1/src/test/java/model/GameTest.java
+++ Assignment1/src/test/java/model/GameTest.java
@@ -1,7 +1,6 @@
 package model;
 
 import org.junit.Test;
-
 import java.util.ArrayList;
 
 import static org.junit.Assert.*;
@@ -17,18 +16,115 @@ public class GameTest {
     }
 
     @Test
-    public void testDeal() {
-
-
-        /*
-        int testint = 0;
-        Game.TableCreator table = new Game.TableCreator();
-        ArrayList<ArrayList<Card>> testtable = table.getTable();
-        Card testcard = new Card(1,1);
-        testtable.get(0).add(testcard);
-        testint = (testtable.get(0).get(0)).rank;
-
-        assertEquals(testint, 1);
-        */
+    public void testDeal1Suit() {
+        Game.TableCreator dealtable = new Game.TableCreator();
+        Deck cards = new Deck();
+        Card card1 = new Card(0, 2);
+        Card card2 = new Card(1, 3);
+        Card card3 = new Card(2, 4);
+        Card card4 = new Card(3, 5);
+        dealtable.table.get(0).add(card1);
+        dealtable.table.get(1).add(card2);
+        dealtable.table.get(2).add(card3);
+        dealtable.table.get(3).add(card4);
+        assertEquals(0, dealtable.table.get(0).get(0).getSuit());
+    }
+    @Test
+    public void testDeal2Suit() {
+        Game.TableCreator dealtable = new Game.TableCreator();
+        Deck cards = new Deck();
+        Card card1 = new Card(0, 2);
+        Card card2 = new Card(1, 3);
+        Card card3 = new Card(2, 4);
+        Card card4 = new Card(3, 5);
+        dealtable.table.get(0).add(card1);
+        dealtable.table.get(1).add(card2);
+        dealtable.table.get(2).add(card3);
+        dealtable.table.get(3).add(card4);
+        assertEquals(1, dealtable.table.get(0).get(0).getSuit());
+    }
+    @Test
+    public void testDeal3Suit() {
+        Game.TableCreator dealtable = new Game.TableCreator();
+        Deck cards = new Deck();
+        Card card1 = new Card(0, 2);
+        Card card2 = new Card(1, 3);
+        Card card3 = new Card(2, 4);
+        Card card4 = new Card(3, 5);
+        dealtable.table.get(0).add(card1);
+        dealtable.table.get(1).add(card2);
+        dealtable.table.get(2).add(card3);
+        dealtable.table.get(3).add(card4);
+        assertEquals(2, dealtable.table.get(0).get(0).getSuit());
+    }
+    @Test
+    public void testDeal4Suit() {
+        Game.TableCreator dealtable = new Game.TableCreator();
+        Deck cards = new Deck();
+        Card card1 = new Card(0, 2);
+        Card card2 = new Card(1, 3);
+        Card card3 = new Card(2, 4);
+        Card card4 = new Card(3, 5);
+        dealtable.table.get(0).add(card1);
+        dealtable.table.get(1).add(card2);
+        dealtable.table.get(2).add(card3);
+        dealtable.table.get(3).add(card4);
+        assertEquals(3, dealtable.table.get(0).get(0).getSuit());
+    }
+    @Test
+    public void testDeal1Rank() {
+        Game.TableCreator dealtable = new Game.TableCreator();
+        Deck cards = new Deck();
+        Card card1 = new Card(0, 2);
+        Card card2 = new Card(1, 3);
+        Card card3 = new Card(2, 4);
+        Card card4 = new Card(3, 5);
+        dealtable.table.get(0).add(card1);
+        dealtable.table.get(1).add(card2);
+        dealtable.table.get(2).add(card3);
+        dealtable.table.get(3).add(card4);
+        assertEquals(2, dealtable.table.get(0).get(0).getRank());
+    }
+    @Test
+    public void testDeal2Rank() {
+        Game.TableCreator dealtable = new Game.TableCreator();
+        Deck cards = new Deck();
+        Card card1 = new Card(0, 2);
+        Card card2 = new Card(1, 3);
+        Card card3 = new Card(2, 4);
+        Card card4 = new Card(3, 5);
+        dealtable.table.get(0).add(card1);
+        dealtable.table.get(1).add(card2);
+        dealtable.table.get(2).add(card3);
+        dealtable.table.get(3).add(card4);
+        assertEquals(3, dealtable.table.get(0).get(0).getRank());
+    }
+    @Test
+    public void testDeal3Rank() {
+        Game.TableCreator dealtable = new Game.TableCreator();
+        Deck cards = new Deck();
+        Card card1 = new Card(0, 2);
+        Card card2 = new Card(1, 3);
+        Card card3 = new Card(2, 4);
+        Card card4 = new Card(3, 5);
+        dealtable.table.get(0).add(card1);
+        dealtable.table.get(1).add(card2);
+        dealtable.table.get(2).add(card3);
+        dealtable.table.get(3).add(card4);
+        assertEquals(4, dealtable.table.get(0).get(0).getRank());
+    }
+    @Test
+    public void testDeal4Rank() {
+        Game.TableCreator dealtable = new Game.TableCreator();
+        Deck cards = new Deck();
+        Card card1 = new Card(0, 2);
+        Card card2 = new Card(1, 3);
+        Card card3 = new Card(2, 4);
+        Card card4 = new Card(3, 5);
+        dealtable.table.get(0).add(card1);
+        dealtable.table.get(1).add(card2);
+        dealtable.table.get(2).add(card3);
+        dealtable.table.get(3).add(card4);
+        assertEquals(5, dealtable.table.get(0).get(0).getRank());
     }
 }
